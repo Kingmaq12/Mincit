e0f4c8f3872ecdcc227a82af0f3dd3777296890a
malejitabm <malejitabm@hotmail.com>
2016-10-04 15:16:56 -0500

    MAS ARREGLOS14

--- src/java/ufps/mincit/datos/conexion/JDBCMiddler.java
+++ src/java/ufps/mincit/datos/conexion/JDBCMiddler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ufps.mincit.negocio.cons;
+package ufps.mincit.datos.conexion;
 
 import java.sql.*;
 import java.util.Iterator;
--- src/java/ufps/mincit/negocio/Negocio.java
+++ src/java/ufps/mincit/negocio/Negocio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ufps.mincit.negocio.business;
+package ufps.mincit.negocio;
 
 /**
  *

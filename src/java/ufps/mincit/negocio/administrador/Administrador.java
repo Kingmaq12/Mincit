b332ce36e4c08d55127138835d096d952b47425a
malejitabm <malejitabm@hotmail.com>
2016-10-18 20:19:57 -0500

    consulta

--- src/java/ufps/mincit/negocio/administrador/Administrador.java
+++ src/java/ufps/mincit/negocio/administrador/Administrador.java
@@ -11,6 +11,7 @@ package ufps.mincit.negocio.administrador;
  */
 import java.util.ArrayList;
 import java.util.Date;
+import ufps.mincit.datos.dao.Entidad_adscritaDAO;
 import ufps.mincit.datos.dao.EventoDAO;
 import ufps.mincit.datos.dao.UsuarioDAO;
 import ufps.mincit.datos.dto.EventoDTO;
@@ -21,51 +22,136 @@ import ufps.mincit.datos.dto.Usuario;
  * @author CESAR
  */
 public class Administrador {
-    
-     public String iniciarSesion(String cedula, String contraseña) throws Exception{
-        String result="";
-        
-        Usuario usu = new Usuario(cedula,contraseña);
+
+    public String iniciarSesion(String cedula, String contraseña) throws Exception {
+        String result = "";
+
+        Usuario usu = new Usuario(cedula, contraseña);
         UsuarioDAO aux = new UsuarioDAO();
-        
-        String usuario =aux.iniciarSesion(usu);
+
+        String usuario = aux.iniciarSesion(usu);
         System.out.println(usuario);
-        if(!usuario.isEmpty()){
-            String[] divi= usuario.split(",");
-            if(divi[3].equalsIgnoreCase("1")){
+        if (!usuario.isEmpty()) {
+            String[] divi = usuario.split(",");
+            if (divi[3].equalsIgnoreCase("1")) {
                 return "S";
             }
-            if(divi[3].equalsIgnoreCase("2")){
+            if (divi[3].equalsIgnoreCase("2")) {
                 return "A";
             }
         }
         return "N";
     }
-    
-     public String registrarEvento(String nombre,String fecha,String hora,String lugar,String patrocinadores,String continente,String pais,String ciudad,String participantes,String tipo_evento,String sector_economico,String url,String imagen, String logros,String descripcion) throws Exception{
-         
-         System.out.println("Negocio, RegistrarEvento");
-         EventoDTO dto = new EventoDTO(nombre, fecha, hora, lugar, patrocinadores, continente, pais, ciudad, participantes, tipo_evento, sector_economico, url, imagen, logros, descripcion);
-         EventoDAO dao = new EventoDAO();
-         
-         boolean resp = dao.registrarEvento(dto);
-         if(resp){
-             return "S";
-         }
-         
-         return "N";
-     }
-     
-          
-     public ArrayList<EventoDTO> consultarEventos(String fecha, String entidad, String pais, String cuidad, String continente, String sector, String logro) throws Exception{
-         
-         if(!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty()&& !cuidad.isEmpty()&& !continente.isEmpty()&& !sector.isEmpty() && !logro.isEmpty()){
-             EventoDTO dto = new EventoDTO ();
-         }
-         if(fecha!=null && entidad.isEmpty() &&pais.isEmpty()&& cuidad.isEmpty()&& continente.isEmpty()&&sector.isEmpty()&&  logro.isEmpty()){
-             
-         }
-         
-         return null;
-     }
+
+    public String registrarEvento(String nombre, String fecha, String hora, String lugar, String patrocinadores, String continente, String pais, String ciudad, String participantes, String tipo_evento, String sector_economico, String url, String imagen, String logros, String descripcion) throws Exception {
+
+        System.out.println("Negocio, RegistrarEvento");
+        EventoDTO dto = new EventoDTO(nombre, fecha, hora, lugar, patrocinadores, continente, pais, ciudad, participantes, tipo_evento, sector_economico, url, imagen, logros, descripcion);
+        EventoDAO dao = new EventoDAO();
+
+        boolean resp = dao.registrarEvento(dto);
+        if (resp) {
+            return "S";
+        }
+
+        return "N";
+    }
+
+    public ArrayList<EventoDTO> consultarEventos(String fecha, String entidad, String pais, String cuidad, String continente, String sector, String logro) throws Exception {
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+            EventoDAO dao = new EventoDAO();
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (!fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (!fecha.isEmpty() && !entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (!fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && !entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && !entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && entidad.isEmpty() && !pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && !pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && !cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && !cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && !continente.isEmpty() && sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && !continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && !sector.isEmpty() && logro.isEmpty()) {
+
+        }
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && !sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        if (fecha.isEmpty() && entidad.isEmpty() && pais.isEmpty() && cuidad.isEmpty() && continente.isEmpty() && sector.isEmpty() && !logro.isEmpty()) {
+
+        }
+
+        return null;
+    }
+
 }
